5e7eb87cbbfb42c7ccf4608fddf4249bf8546a51
John Doe <john.jasper.doe@gmail.com>
2020-06-14 22:33:59 +0300

    Added main function and project options

--- src/bayan.cpp
+++ src/bayan.cpp
@@ -1,7 +1,16 @@
-
 #include <iostream>
+#include <boost/program_options.hpp>
+#include "version.hpp"
 
+void get_param(int argc, char* argv[]) {
+  (void)argc;
+  (void)argv;
+}
 
 int main(int argc, char* argv[]) {
+  std::cout << "BAYAN v." << BAYAN_MAJOR << "." << BAYAN_MINOR << "." << BAYAN_PATCH << std::endl;
+  std::cout << "  Make by Maxim <john.jasper.doe@gmail.com>" << std::endl;
+
+  get_param(argc, argv);
 
 }
