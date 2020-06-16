5141e75703f9bea25d1c69a3a9640222efb26b02
Maxim Gusev <gusev.m@granit-concern.ru>
2020-06-16 12:22:35 +0300

    Added Walker class

---
+++ src/core/walker.hpp
@@ -0,0 +1,22 @@
+/**
+ *
+ *
+ */
+
+#ifndef WALKER_HPP_
+#define WALKER_HPP_
+
+namespace bayan {
+
+class walker {
+
+public:
+  walker() = default;
+
+  void run();
+  void print();
+};
+
+} /* bayan:: */
+
+#endif /* WALKER_HPP_ */
