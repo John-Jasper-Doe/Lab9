6b80198f66ca0fa00fe8c57bb7cee671e4b201f9
Maxim Gusev <gusev.m@granit-concern.ru>
2020-06-29 12:41:09 +0300

    Corrected the remark of teacher No. 1

--- src/bayan.cpp
+++ src/bayan.cpp
@@ -59,6 +59,7 @@ void get_param(int argc, char* argv[]) {
   }
 
   if (!cfg->is_valid()) {
+    std::cout << desc << std::endl;
     throw std::runtime_error("Invalid arguments");
   }
 }
@@ -68,7 +69,13 @@ int main(int argc, char* argv[]) {
   std::cout << "BAYAN v." << BAYAN_MAJOR << "." << BAYAN_MINOR << "." << BAYAN_PATCH << std::endl;
   std::cout << "  Make by Maxim <john.jasper.doe@gmail.com>" << std::endl;
 
-  get_param(argc, argv);
+  try {
+    get_param(argc, argv);
+  }
+  catch (std::runtime_error& e) {
+    std::cout << e.what() << std::endl;
+  }
+
 
   bayan::core::walker w;
   w.run();
